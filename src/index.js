763f0a513dfb09ef6960acdb662c91184065674e
Emerson Silva <emerson_jdss@hotmail.com>
2019-07-04 19:08:14 -0300

    changes on structure

---
+++ src/index.js
@@ -0,0 +1,22 @@
+import React, { Fragment } from 'react';
+import { StyleSheet, View, Text } from 'react-native';
+
+const App = () => {
+  return (
+    <Fragment>
+      <View style={styles.container}>
+        <Text>Teste</Text>
+      </View>
+    </Fragment>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center'
+  },
+});
+
+export default App;
